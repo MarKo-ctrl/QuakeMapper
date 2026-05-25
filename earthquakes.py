4c1eb763503fa39040741a99876a847e6b5f4d53
Marios Kontis <kontism@gmail.com>
2026-05-25 22:59:17 +0100

    Write db queries functions

--- earthquakes.py
+++ earthquakes.py
@@ -1,8 +1,18 @@
 # import pprint
 # from datetime import date
-from lib import get_data
+# from lib import get_data
+from lib import db_queries
 
 # get_data.fetch_all(2000, 2003)
 # get_data.fetch_all(2003, 2005)
 # get_data.fetch_all(2005, 2010)
-get_data.fetch_all(2010, 2026)
+# get_data.fetch_all(2010, 2026)
+
+# eq_2010_2011 = transform.load_by_year_range(2010, 2011)
+# print(eq_2010_2011.head(10))
+
+eq_45_5 = db_queries.load_magnitude_range(4.5, 5.1)
+print(eq_45_5.head(10))
+
+cal_eq = db_queries.load_by_bbox(*(-125.222168,30.845647,-113.884277,39.487085))
+print(cal_eq.shape)
---
+++ lib/db_queries.py
@@ -0,0 +1,49 @@
+# import pandas as pd
+import geopandas as gpd
+# from . import extract
+from .db import get_engine
+
+# ROLE: query and prepare data for analysis
+
+engine = get_engine()
+
+def load_all():
+    '''
+    Load full dataset.
+    '''
+    query = """SELECT *
+            FROM earthquakes"""
+    return gpd.read_postgis(query, engine)
+
+def load_magnitude_range(mag1: float, mag2: float):
+    '''
+    Load events within a magnitude range.
+    Return a GeoDataFrame
+    '''
+    query = """SELECT *
+            FROM earthquakes
+            WHERE magnitude BETWEEN %(mag1)s AND %(mag2)s;"""
+    return gpd.read_postgis(query, engine, params={"mag1":mag1, "mag2":mag2})
+
+def load_by_year_range(year1, year2):
+    '''
+    Load events between two years
+    '''
+    query = """SELECT *
+            FROM earthquakes
+            WHERE year BETWEEN %(year1)s AND %(year2)s;"""
+    return gpd.read_postgis(query, engine, params={"year1":year1,"year2":year2})
+
+def load_by_bbox(min_lon, min_lat, max_lon, max_lat):
+    '''
+    Load events within a bounding box
+    '''
+    query = """SELECT id, year, month, magnitude, depth, tsunami, geom
+            FROM earthquakes,
+            (SELECT ST_MakeEnvelope(%(min_lon)s,%(min_lat)s,%(max_lon)s,%(max_lat)s, 4326) as bbox)
+            WHERE ST_Within(geom, bbox);"""
+    return gpd.read_postgis(query, engine, params={"min_lon":min_lon,
+                                                   "min_lat":min_lat,
+                                                   "max_lon":max_lon,
+                                                   "max_lat":max_lat
+                                                   })
